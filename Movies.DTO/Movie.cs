a71b59babc1ddf9559136803fd0a88c55eba050c
John Poston <john.poston2@outlook.com>
2022-12-10 14:18:34 -0500

    Add project files.

---
+++ Movies.DTO/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Movies.DTO
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Genre { get; set; }
+        public string Title { get; set; }
+        public float Runtime { get; set; }
+    }
+}
